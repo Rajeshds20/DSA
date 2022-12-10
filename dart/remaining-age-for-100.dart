4671e5634c29b427e598f1af9c884dc2c9f36dd0
Rajeshds20 <dsrajesh71@gmail.com>
2022-12-10 11:28:39 +0530

    Changed dart file

--- dart/remaining-age-for-100.dart
+++ dart/remaining-age-for-100.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
+
 void main() {
   print('Enter your Name :');
-  String name = stdin.readLineSync();
+  String? name = stdin.readLineSync();
   int n = int.parse(stdin.readLineSync());
-  n = 100-n;
+  n = 100 - n;
   print('$name, have to go $n years for turning 100');
 }
